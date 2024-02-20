772fe652bb8b612f2c0b09930ff8e56c625acb85
gguille03 <cesarmemo03@gmail.com>
2024-02-19 20:38:34 -0600

    Ejercicio completo

--- app/src/main/java/mx/unam/fi/listacomida/data/DataSource.kt
+++ app/src/main/java/mx/unam/fi/listacomida/data/DataSource.kt
@@ -6,12 +6,12 @@ import mx.unam.fi.listacomida.model.Platillo
 class DataSource {
     fun LoadPlatillos(): List<Platillo> {
         return listOf<Platillo>(
-        Platillo(R.string.desayuno, R.drawable.desayuno, 5.0, true,50.6),
-        Platillo(R.string.hamburger, R.drawable.hamburger, 5.0, true,150.99),
+        Platillo(R.string.desayuno, R.drawable.desayuno, 30.0, true,50.6),
+        Platillo(R.string.hamburger, R.drawable.hamburger, 15.0, false,150.99),
         Platillo(R.string.pizza, R.drawable.pizza, 5.0, true,199.99),
-        Platillo(R.string.postre, R.drawable.postre, 5.0, true,50.99),
-        Platillo(R.string.pozole, R.drawable.pozole, 5.0, true,150.99),
-        Platillo(R.string.tacos, R.drawable.tacos, 5.0, true,30.99),
+        Platillo(R.string.postre, R.drawable.postre, 5.0, false,50.99),
+        Platillo(R.string.pozole, R.drawable.pozole, 8.0, true,150.99),
+        Platillo(R.string.tacos, R.drawable.tacos, 10.0, false,30.99)
         )
     }
 }
